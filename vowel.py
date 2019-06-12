e3d6308ce4f4413f872fe79ba3e7669e2b58fb16
atchu1 <atchayaraj18@gmail.com>
2019-06-12 14:47:21 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,8 +1,10 @@
-str=input()
-for n in str:
-  if(n=='a' or n=='e' or n=='i' or n=='u' or n=='u' or n=='o'):
+s=input()
+for n in s:
+  if(n=='a' or n=='e' or n=='i' or n=='u' or n=='u' or
+n=='A' or n=='E' or n=='I' or n=='O' or n=='U' ):
+    d=1
+if(d==1):
      print("yes")
-     break
-  else:
+else:
     print("no")
-    break
+
